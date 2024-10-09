8ef3987e9ffb8c5f089e657a31d36162b6662867
Tejas P <tejasteju0726@gmail.com>
2024-10-09 17:58:47 +0530

    Add files via upload
    #pythonoperators

---
+++ 4 my program with operators.py
@@ -0,0 +1,17 @@
+a=100
+b=50
+print(a!=b)
+print(a<b)
+print(a==b)
+print(a!=b)
+print(a>=100)
+print(b<=50)
+print(a>45 and b>45)
+print(a>90 or b==50)
+print(not(a==b))
+mylib=[1,5,9,10,11]
+myline="Tejas"
+print(7 in mylib)
+print(10 in mylib)
+print("T" in myline)
+print("H" in myline)
